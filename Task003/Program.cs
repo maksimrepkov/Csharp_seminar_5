79fceb5b79ee37c0ffc6fec48f6ad5ff531375f1
maksimrepkov <maksimrepkov@yandex.ru>
2023-10-25 20:24:57 +0300

    Добавил решение задачи 38 из ДЗ к семинару 5

---
+++ Task003/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача № 38.
+// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] FillArrayRnd()
+{
+    Random rndGenVal = new Random();
+    double[] retVal = new double[rndGenVal.Next(2, 5)];
+    for (int i = 0; i < retVal.Length; i++)
+        retVal[i] = rndGenVal.Next(-100, 100) + rndGenVal.NextDouble();
+    return retVal;
+}
+
+void PrintArray(double[] array)
+{
+    // Console.WriteLine(string.Join("\t", array));
+    Console.WriteLine("[" + string.Join(", ", array) + "]"); 
+}
+
+
+double FindMaxElement(double[] array)
+{
+    double max = array [0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= max)
+        max = array[i];
+    }
+    return max;
+}
+double FindMinElement(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= min)
+        min = array[i];
+    }
+    return min;
+}
+
+double CalcDifferenceBetweenMaxMin(double[] array)
+{
+    double difference = FindMaxElement(array) - FindMinElement(array);
+    return difference;
+}
+
+double[] myArray = FillArrayRnd();
+Console.WriteLine("Массив:");
+PrintArray(myArray);
+Console.WriteLine($"Максимальный элемент массива: {FindMaxElement(myArray)}.");
+Console.WriteLine($"Минимальный элемент массива: {FindMinElement(myArray)}.");
+Console.WriteLine($"Разность между максимальным и минимальным элементом = {CalcDifferenceBetweenMaxMin(myArray)}.");
